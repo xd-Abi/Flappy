f9b8bf70bf44b1c34ec71fc8a345b12c4542f837
xd-Abi <abinayansureskumar@gmail.com>
2022-11-04 19:53:24 +0100

    Added basic mesh generator to generate a quad

---
+++ Flappy/src/main/java/net/xdabi/flappy/util/MeshGenerator.java
@@ -0,0 +1,20 @@
+package net.xdabi.flappy.util;
+
+import net.xdabi.flappy.math.Vec2f;
+import net.xdabi.flappy.math.Vec3f;
+import net.xdabi.flappy.model.Mesh;
+import net.xdabi.flappy.model.Vertex;
+
+public class MeshGenerator {
+
+    public static void createQuad(Mesh mesh) {
+        mesh.create(new Vertex[]{
+                new Vertex(new Vec3f(-0.5f, 0.5f, 0), new Vec2f(0, 0)),
+                new Vertex(new Vec3f(-0.5f, -0.5f, 0), new Vec2f(0, 1)),
+                new Vertex(new Vec3f(0.5f, -0.5f, 0), new Vec2f(1, 1)),
+                new Vertex(new Vec3f(0.5f, 0.5f, 0), new Vec2f(1, 0)),
+        }, new int[]{
+                0, 1, 3, 3, 1, 2
+        });
+    }
+}
